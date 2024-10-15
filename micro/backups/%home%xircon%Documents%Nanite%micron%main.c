13dd583db730c1e0210883543754b5ba37d84b36
Tyler McGurrin <142884506+PKM74@users.noreply.github.com>
2024-10-15 16:55:52 -0400

    Add some config files
    Micro, Neofetch, VLC

---
+++ micro/backups/%home%xircon%Documents%Nanite%micron%main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Varible Setup here
+#define cursor '/>'
+char buffer[255];
+
+
+//Code starts here
+int main()
+	printf("Micron Shell Starting...\n");
+	printf(cursor);
+	scanf(buffer, &s)
+	
+	
+
